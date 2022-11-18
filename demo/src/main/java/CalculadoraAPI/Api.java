35a89cce0efa34dcbfdbac459154d85e232a69fa
AlejandroCisneros19 <aleecisneros19@gmail.com>
2022-11-17 23:09:03 -0300

    Calcu Api en proceso

---
+++ demo/src/main/java/CalculadoraAPI/Api.java
@@ -0,0 +1,30 @@
+package CalculadoraAPI;
+
+public class Api {
+
+    public static int suma(int num, int num2){
+        return num + num2;
+    }
+
+
+
+    public static int resta(int num, int num2){
+        return num - num2;
+    }
+
+
+
+    public static int multi(int num, int num2){
+        return  num * num2;
+    }
+
+
+
+    public static int div(int num, int num2){
+        return num/num2;
+    }
+
+
+
+
+}
---
+++ demo2/src/test/java/CalculadoraAPITest/Test.java
@@ -0,0 +1,12 @@
+package CalculadoraAPITest;
+
+import main.java.CalculadoraApi.Api;
+
+public class Test {
+
+    calcu calcu = new calculator();
+
+
+
+
+}
