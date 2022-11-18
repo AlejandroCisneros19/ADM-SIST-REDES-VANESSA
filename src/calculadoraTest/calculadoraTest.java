40b5f3257387dc1951a4e4e3efa40cacd4561568
AlejandroCisneros19 <aleecisneros19@gmail.com>
2022-11-17 21:11:08 -0300

    items de calcu y test por corregir, listo

--- src/calculadoraTest/calculadoraTest.java
+++ src/calculadoraTest/calculadoraTest.java
@@ -13,44 +13,56 @@ public class calculadoraTest {
     public void sumaTest() {
         assertEquals(calculator.sumar(1, 2), 3);
     }
-
     @Test
     public void sumaTest2(){
         assertNotEquals(calculator.sumar(1,2), 5 );
     }
 
+
+
+
     @Test
     public void restarTest() {
         assertEquals(calculator.restar(2, 2), 0);
     }
-
     @Test
     public void restarTest2(){
         assertNotEquals(calculator.restar(2,2), 3 );
     }
 
+
+
     @Test
     public void multiplicarTest(){
         assertEquals(calculator.multiplicar(3,2), 6);
     }
-
     @Test
     public void multiplicarTest2(){
         assertNotEquals(calculator.multiplicar(3,2), 9);
     }
 
+
+
+
     @Test
     public void dividirTest(){
         assertEquals(calculator.dividir(4,1), 4);
     }
-
     @Test
     public void dividirTest2(){
         assertNotEquals(calculator.dividir(4,1), 5);
     }
 
+    /*division por 0*/
     @Test
-    public void raizCuadraticaTest(){
-        assertNotEquals(calculator.raizCuadratica(2,4,6),-4,2);
-    }
-}
+   public void dividirTest3() { assertNotEquals(calculator.dividir(5,0); }
+
+
+
+    /*raiz negativa*/
+    @Test
+    public void raizCuadraticaTest() { assertEquals(calculadora.cuadratica(1, -1, -2), 2, -1) }
+    @Test
+    public void raizCuadraticaTest() { assertEquals(calculadora.cuadratica( 2, 3, -2), 0.5, -2) }
+
+
